e36f704ce077fe5d2bee48f39dc114e2ab4a5dcf
yasputra <syahputrailyas785@gmail.com>
2026-06-30 11:21:35 +0700

    Menambahkan integrasi Firebase dan endpoint GET pasien

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from app.schemas.patient import Patient
 from app.core.firebase import db
 from app.services.patient_service import create_patient
+from app.services.patient_service import get_all_patients
+from app.services.patient_service import get_patient_by_id
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -14,30 +17,35 @@ def root():
 # crud pasien dulu 
 @app.get("/patients")
 def get_patients():
-    return {
-        "patients": [
-            {
-                "id": 1,
-                "nama": "Budi"
-            },
-            {
-                "id": 2,
-                "nama": "Siti"
-            }
-        ]
-    }
+
+    return get_all_patients()
 
 @app.get("/patients/{patient_id}")
-def get_patient(patient_id: int):
-    return {
-        "id": patient_id,
-        "message": "Data pasien ditemukan"
-    }
+
+def get_patient_by_id(patient_id: str):
+
+    doc = db.collection("patients").document(patient_id).get()
+
+    if not doc.exists:
+        raise HTTPException(
+            status_code=404,
+            detail="Pasien tidak ditemukan"
+        )
+
+    patient = doc.to_dict()
+    patient["id"] = doc.id
+
+    return patient
 
 @app.post("/patients")
 def add_patient(patient: Patient):
 
     return create_patient(patient.model_dump())
+
+@app.get("/patients/{patient_id}")
+def get_patient(patient_id: str):
+
+    return get_patient_by_id(patient_id)
 
 #firebase integrasi
 @app.get("/test-firebase")
--- app/services/patient_service.py
+++ app/services/patient_service.py
@@ -1,5 +1,23 @@
 from app.core.firebase import db
 
+# menampilkan data seluruh pasien
+def get_all_patients():
+
+    patients = []
+
+    docs = db.collection("patients").stream()
+
+    for doc in docs:
+
+        patient = doc.to_dict()
+
+        patient["id"] = doc.id
+
+        patients.append(patient)
+
+    return patients
+
+# menambahkan data pasein
 def create_patient(data: dict):
 
     db.collection("patients").add(data)
@@ -7,3 +25,20 @@ def create_patient(data: dict):
     return {
         "message": "Pasien berhasil ditambahkan"
     }
+
+
+# baca data pasein
+def get_patient_by_id(patient_id: str):
+
+    doc = db.collection("patients").document(patient_id).get()
+
+    if not doc.exists:
+        return {
+            "message": "Pasien tidak ditemukan"
+        }
+
+    patient = doc.to_dict()
+
+    patient["id"] = doc.id
+
+    return patient
